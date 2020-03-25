da03471fefd67688a1d576b9bd60d9a46c160f30
corymaklin <corymaklin@gmail.com>
2020-03-25 18:29:17 -0400

    finished mobile app

--- Details.js
+++ Details.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
-import { Button, Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
+import { Table, Row, Rows } from 'react-native-table-component';
+import _ from 'lodash';
+
+const tableHead = ['Question Number', 'Correct'];
 
 class Details extends Component {
+    
     constructor (props) {
         super(props);
 
@@ -32,15 +37,25 @@ class Details extends Component {
     render () {
         const { questions } = this.state;
 
-        
+        const tableData = _.map(questions, question => {
+            return [question.question_number, question.score.toString()[0].toUpperCase()];
+        });
 
         return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Details Screen</Text>
-
+            <View>
+                <Table borderStyle={{borderWidth: 2, borderColor: '#c8e1ff'}}>
+                    <Row data={ tableHead } style={ styles.head } textStyle={ styles.text } />
+                    <Rows data={ tableData } textStyle={ styles.text }/>
+                </Table>
             </View>
         );
     }
 }
 
 export default Details;
+
+const styles = StyleSheet.create({
+    container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
+    head: { height: 40, backgroundColor: '#f1f8ff' },
+    text: { margin: 6 }
+});
